be7f32e0c15c95d345cc0bf919b9b252cc6660c6
lalitjakhar <lalitkumarjakhar22216@gmail.com>
2023-09-26 13:06:42 +0530

    initial commit

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -8,13 +8,15 @@ const Navbar = () => {
             <section className="navbar_body">
                 <nav className="navbar navbar-expand-lg navbar_body_content">
                     <div className="container">
-                        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarToggler" aria-controls="navbarToggler" aria-expanded="false" aria-label="Toggle navigation">
+                        <button className="navbar-toggler navbar_toggler_btn" type="button" data-bs-toggle="collapse" data-bs-target="#navbarToggler" aria-controls="navbarToggler" aria-expanded="false" aria-label="Toggle navigation">
                             <span className="navbar-toggler-icon"></span>
                         </button>
                         <div className="collapse navbar-collapse" id="navbarToggler">
                             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                                 <li className="nav-item">
-                                    <a className="nav-link" href="#">Home</a>
+                                    <a className="nav-link"
+                                        href="https://en.wikipedia.org/wiki/Mahatma_Gandhi#:~:text=Mohandas%20Karamchand%20Gandhi%20(2%20October,and%20freedom%20across%20the%20world.">
+                                            Home</a>
                                 </li>
                                 <li className="nav-item">
                                     <a className="nav-link" href="#">About</a>
