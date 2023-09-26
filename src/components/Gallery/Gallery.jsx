c88931535ab8ae5efec08163706d1fcc025dc4d3
lalitjakhar <lalitkumarjakhar22216@gmail.com>
2023-09-26 16:54:35 +0530

    initial commit

--- src/components/Gallery/Gallery.jsx
+++ src/components/Gallery/Gallery.jsx
@@ -4,9 +4,13 @@ import './Gallery.css'
 const Gallery = () => {
     return (
         <>
-            <div className="container-fluid gallery_content_body">
-                <div className="gallery_content_heading">
-                    <h2>“Be the change that you wish to see in the world.”</h2>
+            <div className="nav_gallery_content_body">
+                <div className="nav_gallery_content_heading">
+                    <h2>“Be The Change That You Wish To See In The World.”</h2>
+                </div>
+                <div className="nav_gallery_content_para">
+                    <p>
+                        The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from. <br /> <br/>Read more...</p>
                 </div>
             </div>
         </>
