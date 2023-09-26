790658fd25e4e5a4f4eedf95c13be74cfdb43ca4
lalitjakhar <lalitkumarjakhar22216@gmail.com>
2023-09-26 15:10:14 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import Navbar from './components/Navbar/Navbar';
 import About from './components/About/About';
 import Footer from './components/Footer/Footer';
+import Contact from './components/Contact/Contact';
 
 function App() {
   return (
     <>
       <Navbar/>
       <About/>
+      <Contact/>
       <Footer/>
     </>
   );
---
+++ src/components/Contact/Contact.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import './Contact.css';
+const Contact = () => {
+    return (
+        <>
+            <div className="container contact_content_body mb-5">
+                <h1>Contact us</h1>
+                <div className="contact_content_form pt-4">
+                    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Your Name" />
+                </div>
+                <div className="contact_content_form pt-4">
+                    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Your Email" />
+                </div>
+                <div className="contact_content_form pt-4">
+                    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Your Phone Number" />
+                </div>
+                <div className="contact_content_form pt-4">
+                    <textarea rows="4" cols="50" name="comment" className='form-control' form="usrform">
+                        Your Message</textarea>
+                </div>
+                <div className="contact_content_form pt-4">
+                    <form action="#">
+                        <input type="submit" value="Submit"/>
+                    </form>
+                </div>
+            </div>
+        </>
+    );
+}
+
+export default Contact;
