af15f358a7fcb234639f692333096c6a96fefeee
lalitjakhar <lalitkumarjakhar22216@gmail.com>
2023-10-03 10:47:22 +0530

    initial commit

--- src/components/Media/Media.jsx
+++ src/components/Media/Media.jsx
@@ -5,7 +5,7 @@ const Media = () => {
     return (
         <>
             <div className="container media_video_content_body">
-                <iframe width="100%" height="565px" src="https://www.youtube.com/embed/hpZwCRInrgo?si=ah2CU7Qna5iYeN4D" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+                <iframe width="100%" height="565px" src="https://www.youtube.com/embed/P6njRwz_dMw?si=37xG7RNNdRZ5Z7oA" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
             </div>
         </>
     );
